1960890c45a95648274757f112c494c47e555e14
KadiyaRushi <rushi.cilans@gmail.com>
2023-01-23 15:53:40 +0530

    Created Cart And Shop

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import { Nevbar } from "./Components/nevbar";
-
+import { Cart } from "./pages/cart";
+import { Shop } from "./pages/shop";
 function App() {
   return (
     <div className="App">
       <Router>
         <Nevbar />
         <Routes>
-          <Route path="/" />
-          <Route path="/cart" />
+          <Route path="/" element={<Shop />} />
+          <Route path="/cart" element={<Cart />} />
         </Routes>
       </Router>
     </div>
--- src/Components/nevbar.jsx
+++ src/Components/nevbar.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import { ShoppingCart } from "phosphor-react";
+import "./nevbar.css";
 export const Nevbar = () => {
   return (
     <div className="navbar">
       <div className="links">
         <Link to="/">Shop</Link>
-        <Link to="/cart">Cart</Link>
+        <Link to="/cart">
+          <ShoppingCart />
+        </Link>
       </div>
     </div>
   );
